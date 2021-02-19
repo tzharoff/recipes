cb999be9406d515abe314401cf6c2abcc33bddad
tony <tony.zharoff@gmail.com>
2021-02-19 15:56:40 -0800

    IInitial Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+require('dotenv').config();
 
-function App() {
-  return (
+
+const App = () => {
+
+const APP_ID = process.env.APP_ID;
+const APP_KEY = process.env.APP_KEY;
+// https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free
+const exampleReq = 
+  `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+useEffect(() => {
+  console.log('Effect has been run')
+});
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
